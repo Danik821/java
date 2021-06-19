1bc35d5cc9e964a7fe35c27cd145cfd23e6cc4f0
Danik821 <55312931+Danik821@users.noreply.github.com>
2021-06-19 14:55:00 +0300

    Add files via upload

---
+++ portfolio/js/data.js
@@ -0,0 +1,31 @@
+const data = {
+	"first_name": "Daniil",
+	"last_name": "Potrusov",
+	"job_title": "Ученик школы, студент академии",
+	"photo": "img/man.jpg",
+	"phone": "+375 (44) 511-15-22",
+	"email": "fortyplay315@gmail.com",
+	"skype": "skype_number",
+	"linkedin": "link_to_linkedin",
+	"address": "50 Oskina, Gomel",
+	"skills": [
+	"Знаю Adobe Photoshop",
+	"Могу молчать на ВСЕХ языках МИРА",
+	"Знаю Adobe Premier",
+	"Знаю Adobe Ilustraor",
+	"Знаю Scratch 2",
+	"Знаю Constract 2",
+	"Знаю html",
+	"Знаю Английский",
+	"Знаю Python",
+	"Знаю WordPress",
+	],
+	"education": [
+		["2011-2014", "Ясли-Сад №115 города Гомеля"],
+		["2014-2025", "Средняя Школа №2 г.Гомеля имени Г.М.Склезнёва"],
+	],
+		"achievements": [
+		"Браслеты за Экзамены в Академии ШАГ)",
+		],
+		"profile": "Просто позитивный человек которому нравится жить:)"
+	};
